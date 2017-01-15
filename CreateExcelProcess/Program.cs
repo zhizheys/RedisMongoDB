f851c77b50481b0e0bcd16f5b84c6fe354701058
zhizheys <zhizheys@126.com>
2017-01-15 21:57:56 +0800

    add file

--- CreateExcelProcess/Program.cs
+++ CreateExcelProcess/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace CreateExcelProcess
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MS.Common.ExcelHelper;
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            ExcelHelper.CreateInstance().CreateExcel<string>(null,null);
+
         }
     }
 }
---
+++ MS.Common/LogHelper/Log4NetHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace MS.Common.LogHelper
+{
+   public class Log4NetHelper : ILogHelper
+    {
+        log4net.ILog log = log4net.LogManager.GetLogger("testApp.Logging");
+
+        public void Debug(object message)
+        {
+            log.Debug(message);
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            log.Debug(message, exception);
+        }
+
+        public void Info(object message)
+        {
+            log.Info(message);
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            log.Info(message, exception);
+        }
+
+        public void Warn(object message)
+        {
+            log.Warn(message);
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            log.Warn(message, exception);
+        }
+
+        public void Error(object message)
+        {
+            log.Error(message);
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            log.Error(message, exception);
+        }
+
+        public void Fatal(object message)
+        {
+            log.Fatal(message);
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            log.Fatal(message, exception);
+        }
+    }
+}
